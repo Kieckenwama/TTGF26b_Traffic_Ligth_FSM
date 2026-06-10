d6f433be04df72124ee235d37b230c153fdde138
Eric Kiecksee <eronkoburg@gmail.com>
2026-06-10 21:22:19 +0200

    Testbench initial

--- test/tb.v
+++ test/tb.v
@@ -17,18 +17,35 @@ module tb ();
   reg clk;
   reg rst_n;
   reg ena;
-  reg [7:0] ui_in;
   reg [7:0] uio_in;
   wire [7:0] uo_out;
   wire [7:0] uio_out;
   wire [7:0] uio_oe;
+
+  wire  [7:0] ui_in = {7'b0, ped_req}
+
+  // Main road lights
+  wire  main_green = uo_out[0],
+  wire  main_yellow = uo_out[1],
+  wire  main_red = uo_out[2],
+
+  // Side road lights
+  wire  side_green = uo_out[3],
+  wire  side_yellow = uo_out[4],
+  wire  side_red = uo_out[5],
+
+  // Pedestrian lights
+  wire  ped_green = uo_out[6],
+  wire  ped_red = uo_out[7]
+
+
 `ifdef GL_TEST
   wire VPWR = 1'b1;
   wire VGND = 1'b0;
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_example user_project (
+  tt_um_Kieckenwama_Traffic_LIGHT_FSM user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
